d29f629dceeab18c5c5692be60952c3c09c01ad2
Mona Saad <130683461+Monasaad258@users.noreply.github.com>
2024-01-30 21:24:52 +0200

    Add files via upload

---
+++ javascript gd2/main.js
@@ -0,0 +1,82 @@
+// task (1)
+// var a=10;
+// var b=20;
+// var c=15;
+// var e=3;
+// console.log(b%a-c%e);
+
+
+
+
+
+
+
+
+
+// task(2)
+
+// var num = 3;
+// ++num;
+// ++num;
+// ++num;
+// console.log(num); 
+
+// num++;
+// num++;
+// num++;
+// console.log(num); 
+
+// num+=num;
+// console.log(num); 
+
+//  num=num*num*num/num-num;
+// console.log(num); 
+
+// num=num%num+num+num;
+// console.log(num);
+
+// num=num*num-num
+//  console.log(num); 
+
+
+
+
+
+
+
+
+
+
+//task(3)
+
+// var num = "10";
+
+// num=+num + +num;
+// console.log(num); 
+
+// num%=+num+ +num;
+// console.log(num); 
+
+
+
+
+
+
+
+
+
+
+// task (4)
+// var points = 10;
+// // ++points;
+// // ++points;
+// // ++points;
+// // console.log(points); //13
+// --points;
+// --points;
+// console.log(points); //8
+
+
+
+
+
